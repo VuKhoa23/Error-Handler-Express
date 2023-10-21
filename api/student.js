7a6b82cdbd4041964ce6562f8f50407c1b3c29a5
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-10-21 12:09:44 +0700

    finished pseudo code for crud

--- api/student.js
+++ api/student.js
@@ -4,7 +4,37 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send({
-    message: "student",
+    message: "Return all students",
+  });
+});
+
+router.get("/:id", function (req, res, next) {
+  const { id } = req.params;
+
+  res.send({
+    message: `Return student with ${id}`,
+  });
+});
+
+router.post("/", function (req, res, next) {
+  theStudent = req.body;
+  theStudent.message = "This is a create";
+
+  res.send(theStudent);
+});
+
+router.put("/", function (req, res, next) {
+  theStudent = req.body;
+  theStudent.message = "This is a update";
+  console.log(theStudent);
+  res.send(theStudent);
+});
+
+router.delete("/:id", function (req, res, next) {
+  const { id } = req.params;
+
+  res.send({
+    message: `Delete student with ${id}`,
   });
 });
 
