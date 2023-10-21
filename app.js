5dd8e110d84955ce9e2048276e95de8ab1f16618
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-10-21 14:13:41 +0700

    Edit student rest controller folder

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/api/students", studentsApiRouter);
+app.use("/rest_controller/students", studentsApiRouter);
 app.use("/error", intentionalErrorRouter);
 
 // because 404 is thrown by express
